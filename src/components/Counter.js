185c831c773e7fcd4ee96526156905ea002ac6ab
Daniel Cera <dancer6108@students.ecpi.edu>
2024-08-18 06:51:46 -0400

    Updated button behavior, disabled remove button when counter is 0

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import classes from '../css/Counter.module.css';
 
 const Counter = () => {
     const [num, setNum] = useState(0);
 
     return (
-        <div>
-            <h1>{num}</h1>
-            <div>
-                <button onClick={() => setNum(num + 1)}>++</button>
-                <button onClick={() => (num > 0) ? setNum(num - 1) : null}>--</button>
+        <div className={classes.container}>
+            <h1 className={classes.counter}>{num}</h1>
+            <div className={classes.button_cont}>
+                <button className={classes.button} onClick={() => setNum(num + 1)}>Add 1</button>
+                {num > 0 ?
+                    <button className={classes.button} onClick={() => setNum(num - 1)}>Remove 1</button> :
+                    <button className={classes.button_dis}>Remove 1</button>}
             </div>
         </div>
     );
