a82878650a40c04dfcbb32eb7ea8ab4653a603ed
Daniel Cera <dancer6108@students.ecpi.edu>
2024-08-18 07:00:37 -0400

    rollback of bad update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Counter from './components/Counter';
 import classes from './css/App.module.css';
-import Bad from './a_bad_change_i_dont_want';
 
 export default function App() {
   return ( 
       <div className={classes.container}>
         <Counter />
-        <Bad />
       </div>
   );
 };
--- src/a_bad_change_i_dont_want.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const bad = () => {
-    return <p>Bad Change.</p>;
-}
-
-export default bad;
